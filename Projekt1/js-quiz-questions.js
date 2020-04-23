803ae6ba078e4c655c5ba3ba4ef1fad038965f6a
danielnoukas <44188552+danielnoukas@users.noreply.github.com>
2020-04-23 13:11:55 +0300

    Update js-quiz-questions.js

--- Projekt1/js-quiz-questions.js
+++ Projekt1/js-quiz-questions.js
@@ -3,53 +3,210 @@
 // In the format of {q: QUESTION, o: OPTIONS, a: CORRECT ANSWER}
 var questions = [
     {
-      q : "What is the standard distance between the target and archer in Olympics?",
+      q : "Mis on Venemaa pealinn?",
       o : [
-        "50 meters",
-        "70 meters",
-        "100 meters",
-        "120 meters"
+        "Berliin",
+        "Moskva",
+        "Vilnius",
+        "Varssavi"
       ],
       a : 1 // arrays start with 0, so it is 70 meters
     },
     {
-      q : "Which is the highest number on a standard roulette wheel?",
+      q : "Mis keelt kasutab Monaco?",
       o : [
-        "22",
-        "24",
-        "32",
-        "36"
+        "Vene",
+        "Inglise",
+        "Eesti",
+        "Prantsuse"
       ],
       a : 3
     },
     {
-      q : "How much wood could a woodchuck chuck if a woodchuck would chuck wood?",
+      q : "Millise riigi pealinn on Berliin?",
       o : [
-        "400 pounds",
-        "550 pounds",
-        "700 pounds",
-        "750 pounds"
+        "Kreeka",
+        "Leedu",
+        "Saksamaa",
+        "Holland"
       ],
       a : 2
     },
     {
-      q : "Which is the seventh planet from the sun?",
+      q : "Läti pealinnaks on...?",
       o : [
-        "Uranus",
-        "Earth",
-        "Pluto",
-        "Mars"
+        "Riia",
+        "Valletta",
+        "Viin",
+        "Tirana"
       ],
       a : 0
     },
     {
-      q : "Which is the largest ocean on Earth?",
+      q : "Mis riik kasutab itaalia keelt?",
       o : [
-        "Atlantic Ocean",
-        "Indian Ocean",
-        "Arctic Ocean",
-        "Pacific Ocean"
+        "Island",
+        "Soome",
+        "Serbia",
+        "San Marino"
       ],
       a : 3
-    }
+    },
+    {
+      q : "Millise riigi pealinn on Dublin?",
+      o : [
+        "Sebia",
+        "Rootsi",
+        "Norra",
+        "Iirimaa"
+      ],
+      a : 3
+    },
+    {
+      q : "Riigi keeleks on kasahhi, mis riigiga on tegemist?",
+      o : [
+        "Kreeka",
+        "Kasahstan",
+        "Itaalia",
+        "Holland"
+      ],
+      a : 1
+    },
+    {
+      q : "Riigi pealinnaks on Oslo, mis riigiga on tegemist?",
+      o : [
+        "Norra",
+        "Soome",
+        "Serbia",
+        "Albaania"
+      ],
+      a : 0
+    },
+    {
+      q : "Mis on Eesti pealinn?",
+      o : [
+        "Narva",
+        "Tartu",
+        "Tallinn",
+        "Ei olegi pealinna"
+      ],
+      a : 2
+    },
+    {
+      q : "Millise riigi keeleks on katalaani?",
+      o : [
+        "Horvaatia",
+        "Austria",
+        "Albaania",
+        "Andorra"
+      ],
+      a : 3
+    },
+    {
+      q : "Millist riik kasutab saksa keelt?",
+      o : [
+        "Austria",
+        "Kreeka",
+        "Monaco",
+        "Serbia"
+      ],
+      a : 0
+    },
+    {
+      q : "Millise riigi pealinnaks on Belgrad?",
+      o : [
+        "Slovakkia",
+        "Serbia",
+        "San Marino",
+        "Monaco"
+      ],
+      a : 1
+    },
+    {
+      q : "milline pealinn on Islandil?",
+      o : [
+        "Ljubljana",
+        "Skopje",
+        "Vaduz",
+        "Reykjavik"
+      ],
+      a : 3
+    },
+    {
+      q : "Albaania pealinn on... ?",
+      o : [
+        "Thbilisi",
+        "Jerevan",
+        "Tirana",
+        "Pole pealinna"
+      ],
+      a : 2
+    },
+    {
+      q : "Mis keel kasutab Ungari?",
+      o : [
+        "Ungari keelt",
+        "Eesti keelt",
+        "Prantsuse keelt",
+        "Montenegro keelt"
+      ],
+      a : 0
+    },
+    {
+      q : "Leedu pealinnaks on?",
+      o : [
+        "Rooma",
+        "Oslo",
+        "Riia",
+        "Vilnius"
+      ],
+      a : 3
+    },
+    {
+      q : "Mis keelt kasutab Valgevene?",
+      o : [
+        "Eesti",
+        "Taani",
+        "Rootsi",
+        "Vene"
+      ],
+      a : 3
+    },
+    {
+      q : "Millise riigi pealinnaks on Zagreb?",
+      o : [
+        "Albaania",
+        "Gruusia",
+        "Horvaatia",
+        "Bulgaaria"
+      ],
+      a : 2
+    },
+    {
+      q : "Mis keelt kasutab Andorra?",
+      o : [
+        "Itaalia",
+        "Gruusia",
+        "Armeenia",
+        "Katalaani"
+      ],
+      a : 3
+    },
+    {
+      q : "Millise riigi pealinnaks on Kopenhaagen?",
+      o : [
+        "Taani",
+        "Eesti",
+        "Rootsi",
+        "Vatikan"
+      ],
+      a : 0
+    },
+
+
+
+
+
+
+
   ];
